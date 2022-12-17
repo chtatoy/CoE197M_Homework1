6c41a8d38e3c755e34968de77b627e901a5a7be6
chtatoy <112958384+chtatoy@users.noreply.github.com>
2022-12-17 20:45:31 +0800

    modified:   HW2_Polynomial-Solver/solver.py

--- HW2_Polynomial-Solver/solver.py
+++ HW2_Polynomial-Solver/solver.py
@@ -5,15 +5,18 @@
 # Homework 2:
 from tinygrad.tensor import Tensor
 import tinygrad.nn.optim as optim
-import csv
+# import csv
+import pandas as pd
 
 # Use data_train.csv to estimate the degree and coefficients of a polynomial
-a = open('data_train.csv','r')
-data_train = csv.reader(a)
+# a = open('data_train.csv','r')
+# data_train = csv.reader(a)
 
 # Use data_test.csv to test the generalization of the learned function
-b = open('data_test.csv','r')
-data_test = csv.reader(b)
+# b = open('data_test.csv','r')
+# data_test = csv.reader(b)
+
+# --------------------------------------------------
 
 class TinyBobNet:
   def __init__(self):
@@ -26,9 +29,9 @@ class TinyBobNet:
 model = TinyBobNet()
 optim = optim.SGD([model.l1, model.l2], lr=0.001)
 
-# ... and complete like pytorch, with (x,y) data
-x = data_train
-y = data_test
+df = pd.read_csv('data_train.csv')
+x = df["x"]
+y = df["y"]
 
 out = model.forward(x)
 loss = out.mul(y).mean()
@@ -36,5 +39,7 @@ optim.zero_grad()
 loss.backward()
 optim.step()
 
-a.close()
-b.close()
+# --------------------------------------------------
+
+# a.close()
+# b.close()
