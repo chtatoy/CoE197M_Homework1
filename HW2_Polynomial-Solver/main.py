632e78fee2e8624229e36cf539c0ec74840a7d36
chtatoy <112958384+chtatoy@users.noreply.github.com>
2022-12-17 17:05:38 +0800

    test tinygrad

--- HW2_Polynomial-Solver/main.py
+++ HW2_Polynomial-Solver/main.py
@@ -3,3 +3,24 @@
 # CoE197M-THY
 
 # Homework 2:
+from tinygrad.tensor import Tensor
+import tinygrad.nn.optim as optim
+
+class TinyBobNet:
+  def __init__(self):
+    self.l1 = Tensor.uniform(784, 128)
+    self.l2 = Tensor.uniform(128, 10)
+
+  def forward(self, x):
+    return x.dot(self.l1).relu().dot(self.l2).logsoftmax()
+
+model = TinyBobNet()
+optim = optim.SGD([model.l1, model.l2], lr=0.001)
+
+# ... and complete like pytorch, with (x,y) data
+
+out = model.forward(x)
+loss = out.mul(y).mean()
+optim.zero_grad()
+loss.backward()
+optim.step()
