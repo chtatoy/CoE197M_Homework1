dd752b9e8a6ab58cc958a3d8d43778c92eeb3e5e
chtatoy <112958384+chtatoy@users.noreply.github.com>
2022-12-17 23:06:00 +0800

    modified:   HW2_Polynomial-Solver/solver.py

--- HW2_Polynomial-Solver/solver.py
+++ HW2_Polynomial-Solver/solver.py
@@ -7,17 +7,29 @@ from tinygrad.tensor import Tensor
 import tinygrad.nn.optim as optim
 # import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+# import numpy as np
+from sympy import *
+
+
+# Data Preprocessing
 
 # Use data_train.csv to estimate the degree and coefficients of a polynomial
-# a = open('data_train.csv','r')
-# data_train = csv.reader(a)
+df = pd.read_csv('data_train.csv')
+x_train = df["x"]
+y_train = df["y"]
 
 # Use data_test.csv to test the generalization of the learned function
-# b = open('data_test.csv','r')
-# data_test = csv.reader(b)
+df = pd.read_csv('data_test.csv')
+x_test = df["x"]
+y_test = df["y"]
 
-# --------------------------------------------------
+# ---
 
+# Fitting a Model
+
+# Backward propagation
+# Model using tinygrad.nn.optim.SGD
 class TinyBobNet:
   def __init__(self):
     self.l1 = Tensor.uniform(784, 128)
@@ -27,19 +39,73 @@ class TinyBobNet:
     return x.dot(self.l1).relu().dot(self.l2).logsoftmax()
 
 model = TinyBobNet()
-optim = optim.SGD([model.l1, model.l2], lr=0.001)
+optim = optim.SGD([model.l1, model.l2], lr=0.001)   # learning rate = 0.001
 
-df = pd.read_csv('data_train.csv')
-x = df["x"]
-y = df["y"]
-
-out = model.forward(x)
-loss = out.mul(y).mean()
+out = model.forward(x_train)
+loss = out.mul(y_train).mean()
 optim.zero_grad()
 loss.backward()
 optim.step()
 
-# --------------------------------------------------
+# hypothesis function, h
+# h_θ(x) = θ_0 + θ_1*x_1
+
+# Predict Coefficient
+train_pred = out.predict(x_train)
+test_pred = out.predict(x_test)
+
+diff1 = []
+diff1 = x_train
+diff2 = []
+# Predict Degree
+# an n-degree polynomial has at most n x-intercepts and n-1 turning pts
+# to find degree of polynomial with data, find level where "difference" is constant
+# while True:
+#   for c in len(diff1):
+#     if c == 0:
+#       subt = diff1[c]
+#     diff2[c-1] = diff1[c]-subt
+#     subt = diff1[c]
+#     i+=1
+#   if diff1[0]==diff2[0]:
+#     break
+# degree = i
+
+# degree of a function can be computed as deg f(x) = lim_x→∞ (x*f'(x))/f(x)
+x = x_train
+y = y_train
+z = y.matmul(x).sum()
+degree = limit(y.grad,x.grad,oo)
+
+# ---
+
+# Visualizing Results of Model
+
+# Plot
+plt.scatter(x_train, y_train, c='blue', label='train x')
+plt.scatter(x_test, y_test, c='green', label='test x')
+plt.scatter(x_train, train_pred, c='red', label='train pred')
+plt.scatter(x_test, test_pred, c='orange', label='test pred')
+plt.legend()
+plt.show()
+
+# ---
 
 # a.close()
 # b.close()
+
+# --------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+# github.com/geohot/tinygrad
+# stackoverflow.com/questions/70638278/training-a-neural-network-to-learn-polynomial-equation
+# askpython.com/python/examples/polynomial-regression-in-python
+# https://rickwierenga.com/blog/ml-fundamentals/polynomial-regression.html
